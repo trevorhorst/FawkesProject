107ce2dc9356ec5846ffb7e35e38191a2f9c6219
Trevor Horst <trevorhorst1212@gmail.com>
2020-10-15 14:37:51 -0400

    Adding logger to the core library.

---
+++ fawkeslib/core/include/fawkes/core/logger/log.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2017 rxi
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `log.c` for details.
+ */
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#define LOG_VERSION "0.1.0"
+
+#define LOG_USE_COLOR
+
+typedef void (*log_LockFn)(void *udata, int lock);
+
+enum { LOG_TRACE, LOG_DEBUG, LOG_INFO, LOG_WARN, LOG_ERROR, LOG_FATAL };
+
+#define LOG_TRACE(...) log_log(LOG_TRACE, __FILENAME__, __LINE__, __VA_ARGS__)
+#define LOG_DEBUG(...) log_log(LOG_DEBUG, __FILENAME__, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...)  log_log(LOG_INFO,  __FILENAME__, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...)  log_log(LOG_WARN,  __FILENAME__, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) log_log(LOG_ERROR, __FILENAME__, __LINE__, __VA_ARGS__)
+#define LOG_FATAL(...) log_log(LOG_FATAL, __FILENAME__, __LINE__, __VA_ARGS__)
+
+void log_set_udata(void *udata);
+void log_set_lock(log_LockFn fn);
+void log_set_fp(FILE *fp);
+void log_set_level(int level);
+void log_set_quiet(int enable);
+
+void log_log(int level, const char *file, int line, const char *fmt, ...);
+
+#endif
--- fawkeslib/example/main.cpp
+++ fawkeslib/example/main.cpp
@@ -8,15 +8,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LOG_USE_COLOR
+
 #include "fawkes/core/json/cjson.h"
+#include "fawkes/core/logger/log.h"
 
 int main( /* int argc, char *argv[] */ )
 {
     cJSON *test = cJSON_CreateObject();
     cJSON_AddStringToObject( test, "hello", "world" );
 
-    char *hello = cJSON_Print( test );
-    printf( "%s\n", hello );
+    char *hello = cJSON_PrintUnformatted( test );
+    LOG_INFO( "%s", hello );
 
     free( hello );
     cJSON_Delete( test );
